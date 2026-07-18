cf4cac8dad9f3d49e18eea6eb0e68fc479710cb6
Clement Dhote <clement.dhote@gmail.com>
2026-07-18 20:11:53 +0200

    ups

--- robot/hardware/ups_hat.py
+++ robot/hardware/ups_hat.py
@@ -7,21 +7,22 @@ class UPSHat:
         self.bus=SMBus(1)
 
     def read(self,reg): return self.bus.read_byte_data(self.ADDRESS,reg)
-
     def read16(self,reg): return self.read(reg)|(self.read(reg+1)<<8)
 
-    def battery(self,index):
-        return self.read16(0x30+index*2)
+    def battery(self,index): return self.read16(0x30+index*2)
+    def batteries(self): return [self.battery(i) for i in range(4)]
 
-    def batteries(self):
-        return [self.battery(i) for i in range(4)]
+    def voltage(self): return self.read16(0x20)/1000
+    def current(self):
+        value=self.read16(0x22)
+        if value>=32768: value-=65536
+        return value
 
-    def voltage(self):
-        cells=self.batteries()
-        return sum(cells)/1000
+    def percentage(self): return self.read16(0x24)
+    def remaining_capacity(self): return self.read16(0x26)
+    def remaining_discharge(self): return self.read16(0x28)
+    def remaining_charge(self): return self.read16(0x2a)
 
     def charging(self): return bool(self.read(0x02)&0x80)
-
-    def usb_present(self): return bool(self.read(0x02)&0x20)
-
     def fast_charge(self): return bool(self.read(0x02)&0x40)
+    def usb_present(self): return bool(self.read(0x02)&0x20)
--- robot/tests/test_ups.py
+++ robot/tests/test_ups.py
@@ -2,7 +2,12 @@ from robot.hardware.ups_hat import UPSHat
 
 ups=UPSHat()
 
-print("Cells:",ups.batteries())
-print("Pack :",ups.voltage(),"V")
-print("USB  :",ups.usb_present())
-print("Charge:",ups.charging())
+print("Cells      :",ups.batteries())
+print("Voltage    :",ups.voltage(),"V")
+print("Current    :",ups.current(),"mA")
+print("Battery    :",ups.percentage(),"%")
+print("Remaining  :",ups.remaining_capacity(),"mAh")
+print("Discharge  :",ups.remaining_discharge(),"min")
+print("Charge     :",ups.remaining_charge(),"min")
+print("USB        :",ups.usb_present())
+print("Charging   :",ups.charging())
