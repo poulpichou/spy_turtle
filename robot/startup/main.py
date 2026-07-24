90f433201f69bfaffd694a849cd905b26a3c972e
Clement Dhote <clement.dhote@gmail.com>
2026-07-24 22:18:48 +0200

    Add temporary LED hardware test

--- robot/startup/main.py
+++ robot/startup/main.py
@@ -6,33 +6,29 @@ from robot.system.runtime import set_robot
 from robot.config import settings
 from robot.utils.logger import log
 
-
 def start_api():
     import uvicorn
     from robot.api.app import app
-
     log.info("Starting API")
     uvicorn.run(app,host=settings.API_HOST,port=settings.API_PORT)
 
-
 def main():
     log.info("Starting Spy Turtle")
-
     robot=RobotFactory(simulation=settings.SIMULATION).create()
     set_robot(robot)
 
+    # TEMPORARY HARDWARE TEST: keep all 32 LEDs solid red.
+    robot.leds.set_mode("red")
+    robot.state.led_mode="red"
+    log.info("[LED TEST] 32 LEDs set to solid red")
+
     log.info("Robot ready")
-
     threading.Thread(target=start_api,daemon=True).start()
-
     delay=1/settings.UPDATE_RATE
-
     log.info("Main loop started")
-
     while True:
         robot.update()
         time.sleep(delay)
-
 
 if __name__=="__main__":
     main()
